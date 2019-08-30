3cb58e1cf2ba398959034196e2ad5cf404014029
Константин Дьяченко <konard@me.com>
2019-08-30 21:46:08 +0300

    Added missing comments.

--- Performance.cs
+++ Performance.cs
@@ -2,12 +2,23 @@
 using System.Diagnostics;
 using System.Runtime.CompilerServices;
 
-#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
-
 namespace Platform.Diagnostics
 {
+    /// <summary>
+    /// <para>Provides a set of methods for performance measurement.</para>
+    /// <para>Предоставляет набор методов для замера производительности.</para>
+    /// </summary>
     public static class Performance
     {
+        /// <summary>
+        /// <para>Measures the time it takes to execute the action by executing it and using <see cref="Stopwatch"/>.</para>
+        /// <para>Измеряет время, которое занимает выполнение действия, выполняя его и используя <see cref="Stopwatch"/>.</para>
+        /// </summary>
+        /// <param name="action"><para>The <see cref="Action"/> delegate.</para><para>Делегат <see cref="Action"/>.</para></param>
+        /// <returns>
+        /// <para>The time interval representing the time it took to execute the action.</para>
+        /// <para>Временной интервал, представляющий время, которое потребовалось для выполнения действия.</para>
+        /// </returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static TimeSpan Measure(Action action)
         {
